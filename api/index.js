b02199734d4b53d33f4f7fd74d5b97c81958093e
rohitashgoswami <rohitash0520@gmail.com>
2026-04-26 19:32:52 +0530

    Fix Express app export for Vercel serverless

--- api/index.js
+++ api/index.js
@@ -1,9 +1,5 @@
 import { createApp } from "../server/src/app.js";
-import { env } from "../server/src/config/env.js";
 
 const app = createApp();
 
-export default async function handler(req, res) {
-  // On Vercel, we always use fallback products (no database needed)
-  return app(req, res);
-}
+export default app;
